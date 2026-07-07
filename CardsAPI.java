96dcb77daba0e6868f75c05a414f2751ad9613ff
AsmArt1st <hebus994@gmail.con>
2026-07-07 19:33:31 -0400

    reformatted shuffle and deal methods to exist in Deck class

--- CardsAPI.java
+++ CardsAPI.java
@@ -20,49 +20,47 @@ public class CardsAPI {
 
   //creating deck
     public static class Deck {
-      public ArrayList<Card> deckOfCards;
+      public ArrayList<Card> cards;
       public Deck() {
-        deckOfCards = new ArrayList<>(52);
+        cards = new ArrayList<>(52);
         String[] suits = {"Spades", "Hearts", "Diamonds", "Clubs"};
         String[] values = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
         for (String suit : suits) {
           for (String val : values) {
-            deckOfCards.add(new Card(val, suit));
+            cards.add(new Card(val, suit));
           }
         }
       }
+                
+      //Shuffle cmd
+      public void shuffle(){
+        Collections.shuffle(cards);
+      }
+
+      //Dealing cmd
+      public void deal(ArrayList<Card> role, int dealSize) {
+        for (int i = 0; i < dealSize; i++) {
+          Card topCard = cards.remove(0);
+          role.add(topCard);
+        }
+      }
     }
-
-
-  static Deck deck = new Deck();
-
-  //Shuffle cmd
-  public static void shuffle(){
-    Collections.shuffle(deck.deckOfCards);
-  }
-
-  //Dealing cmd
-  public static void deal(ArrayList<Card> role, int dealSize) {
-    for (int i = 0; i < dealSize; i++) {
-      Card topCard = deck.deckOfCards.remove(0);
-      role.add(topCard);
-    }
-  }
 
   //test
   public static void main(String[] args){
     ArrayList<Card> dealerHand = new ArrayList<>();
     ArrayList<Card> playerHand = new ArrayList<>();
-    CardsAPI.shuffle();
+    Deck testDeck = new Deck();
+    testDeck.shuffle();
     int gameHandSize = 5;
     System.out.println("---Player Hand---");
-    CardsAPI.deal(playerHand, gameHandSize);
+    testDeck.deal(playerHand, gameHandSize);
     for (Card card : playerHand) {
       System.out.println(card);
     }
     System.out.println();
     System.out.println("---Dealer Hand---");
-    CardsAPI.deal(dealerHand, gameHandSize);
+    testDeck.deal(dealerHand, gameHandSize);
     for (Card card : dealerHand) {
     System.out.println(card);
     }
