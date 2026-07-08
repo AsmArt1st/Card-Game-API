6e2c9a9cb4bd6d5118f3b778c561106f821bc08c
AsmArt1st <hebus994@gmail.con>
2026-07-08 02:15:33 -0400

    Encapsulated hand fields and made/improved getter methods

--- src/Hand.java
+++ src/Hand.java
@@ -3,24 +3,29 @@ import java.util.*;
 
 public class Hand {
 
-    ArrayList<Card> cards;
-    String handName;
+    private List<Card> cards;
+    private String handName;
 
     public Hand(String handName) {
         cards = new ArrayList<Card>();
         this.handName = handName;
     }
 
-    public void addCards(Card card) {
+    //hand commands
+    public void addCard(Card card) {
         cards.add(card);
     }
 
-    public void removeCards(Card card) {
+    public void removeCard(Card card) {
         cards.remove(card);
     }
 
-    public ArrayList<Card> getCards() {
-        return cards;
+    public List<Card> getCards() {
+        return Collections.unmodifiableList(cards);
+    }
+
+    public String getHandName() {
+        return handName;
     }
 
     //hand string formatting
